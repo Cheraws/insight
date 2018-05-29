bd19a21ea178a22bbbc34e1531008da4b0d00fca
Cheraws <robert@Roberts-MacBook-Pro-4.local>
2018-05-29 00:30:14 -0700

    Added a readme

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-#Insight Coding Challenge
+# Insight Coding Challenge
+
+# Approach
+
+## Data Structures
+I have two structures that I primarily use to solve this problem.
+
+The first data structure I use is an OrderedDict called userinfo. The key is the IP of the user, while the value contains a UserData object
+that represents the user's information. In this object, I keep track of the current time, the most recent time the user accessed something, 
+and the number of requests that he has made.
+
+The second data structure I use is a dictionary called time heaps. The key of the dictionary is the time of access. The value contains a heap that 
+represents all accesses made by all users within that timeframe. The heap is sorted by when the user has first accessed the file to account for the case
+where users are printed in order by first time access as a tiebreaker. 
+
+## The Algorithm
+Each request is read one after the other. If the request isn't already in the userinfo dictionary, add the request to it. If it is already in there, change the 
+most recent access to that time and increase the number of requests by 1. We add this value to a time bucket containing all the accesses made by any user at that time. After we insert the first user's information, we make a check in the second data structure. There is a variable sessionWindowStart that is initialized as the very first access time of EDGAR. We increment the sessionWindowStart until the sessionWindowStart plus the inactivity time is greater than the current time of the current request. In each bucket represnting a time, we compare the logs in there with the userinfo dictionary. If the logs in the bucket have the same ending time as the data in the userinfo dictionary, we print the user's logging info and delete him from user info.
+
+Once we reach the end of the file, we have to print all of the files. We go through the userinfo dictionary, printing all of the users that have not yet expired in the order that they accessed files. 
+
+
+# Directory Structure
+
+    ├── README.md
+    ├── run.sh
+    ├── src
+    │   └── sessionization.py
+    ├── input
+    │   └── inactivity_period.txt
+    │   └── log.csv
+    ├── output
+    |   └── sessionization.txt
+    ├── insight_testsuite
+        └── run_tests.sh
+        └── tests
+            └── test_1
+            |   ├── input
+            |   │   └── inactivity_period.txt
+            |   │   └── log.csv
+            |   |__ output
+            |   │   └── sessionization.txt
+Various extra tests within the test folder.
--- insight_testsuite/temp/src/sessionization.py
+++ insight_testsuite/temp/src/sessionization.py
@@ -6,81 +6,106 @@ import datetime
 import collections
 import heapq
 from queue import Queue
-from datetime import timedelta 
+from datetime import timedelta
 
-#Data sturture that is stored inside a minheap. Contains the ip, requests, and the order it was accessed in.
+# Data sturture that is stored inside a minheap. Contains the ip,
+# requests, and the order it was accessed in.
+
+
 class UserTime:
-    def __init__(self, ip,requests,logs,current_time):
+    def __init__(self, ip, logs):
         self.ip = ip
-        self.requests = requests
         self.logs = logs
-        self.current_time = current_time
 
-    def __lt__(self,other):
+    def __lt__(self, other):
         return self.logs < other.logs
 
-#metadata of the user. Contains current_time, requests, and the IP of the user.
+# metadata of the user. Contains current_time, requests, and the IP of the
+# user.
+
+
 class UserData:
     total_logs = 0
-    def __init__(self,current_time,requests,ip):
+
+    def __init__(self, current_time, requests, ip):
         self.logs = UserData.total_logs + 1
         UserData.total_logs = self.logs
         self.current_time = current_time
         self.end_time = current_time
         self.requests = requests
         self.ip = ip
-    
+
     def __lt__(self, other):
         return self.logs < other.logs
 
-#time conversion to a form that Python can utilize.
+# time conversion to a form that Python can utilize.
+
+
 def convertTime(log_date, log_time):
     f = "%Y-%m-%d %H:%M:%S"
     d_str = '{0} {1}'.format(log_date, log_time)
     time = datetime.datetime.strptime(d_str, f)
     return time
 
-def checkInactiveHeap(time_heaps, old_time, user_info,inactivity_time,current_time,final=False):
-    #Go through queue until the time gap is less than the inactivity timer.
-    while old_time < current_time:
-        if old_time + timedelta(seconds=inactivity_time) >= current_time:
-            return old_time
-        else:
-            if old_time in time_heaps:
-                while time_heaps[old_time]:
-                    data = heapq.heappop(time_heaps[old_time])
-                    ip = data.ip
-                    #if maximum request one already filtered out.
-                    if ip not in  user_info:
-                        continue
-                    if data.current_time == user_info[ip].end_time:
-                        printUserData(user_info[ip],ip)
-                        del(user_info[ip])
-                #save time of old_time
-                del time_heaps[old_time]
-        old_time = old_time + timedelta(seconds=1)
-    return old_time
 
-#outputs userdata to the file location given.
-def printUserData(user_data,ip):
+def advanceSessionWindow(
+        time_heaps,
+        session_window_start,
+        user_info,
+        inactivity_time,
+        current_time,
+        final=False):
+    # Go through queue until the time gap is less than the inactivity timer.
+    while session_window_start < current_time:
+        if session_window_start + timedelta(seconds=inactivity_time) >= current_time:
+            return session_window_start
+        else:
+            if session_window_start in time_heaps:
+                while time_heaps[session_window_start][0]:
+                    data = heapq.heappop(time_heaps[session_window_start][0])
+                    ip = data.ip
+                    # if maximum request one already filtered out.
+                    if ip not in user_info:
+                        continue
+                    if session_window_start == user_info[ip].end_time:
+                        printUserData(user_info[ip], ip)
+                        del(user_info[ip])
+                # save time of session_window_start
+                del time_heaps[session_window_start]
+        session_window_start = session_window_start + timedelta(seconds=1)
+    return session_window_start
+
+# outputs userdata to the file location given.
+
+
+def printUserData(user_data, ip):
     start_time = user_data.current_time
     end_time = user_data.end_time
     start_time_string = "{:%Y-%m-%d %H:%M:%S}".format(start_time)
     end_time_string = "{:%Y-%m-%d %H:%M:%S}".format(end_time)
     seconds = int((end_time - start_time).total_seconds() + 1)
     requests = user_data.requests
-    return_text = ("{0},{1},{2},{3},{4}".format(ip, start_time_string,end_time_string,
-        seconds, requests))
-    #print(return_text)
-    with open(sys.argv[3],"a") as output_file:
+    return_text = (
+        "{0},{1},{2},{3},{4}".format(
+            ip,
+            start_time_string,
+            end_time_string,
+            seconds,
+            requests))
+    # print(return_text)
+    with open(sys.argv[3], "a") as output_file:
         output_file.write(return_text + '\n')
-            
-#Go through all the keys in order in the OrderedDict of users and finish printing up everything.
-def checkNaiveInactive(user_times, inactivity_time,current_time,final=False):
+        output_file.close()
+
+# Go through all the keys in order in the OrderedDict of users and finish
+# printing up everything.
+
+def checkNaiveInactive(user_times, inactivity_time, current_time, final=False):
     deleted_users = []
     for user in user_info.keys():
-        if user_info[user].end_time + timedelta(seconds=inactivity_time) < current_time or final == True:
-            printUserData(user_info[user],user)
+        if user_info[user].end_time + \
+                timedelta(seconds=inactivity_time) < current_time or final:
+            printUserData(user_info[user], user)
             deleted_users += [user]
     for user in deleted_users:
         del(user_info[user])
@@ -89,47 +114,46 @@ def checkNaiveInactive(user_times, inactivity_time,current_time,final=False):
 if __name__ == "__main__":
     user_info = collections.OrderedDict()
     time_heaps = {}
-    user_times = Queue()
     start = None
-    inactivity_time = 2
+    inactivity_time = None
     oldest_entry = None
-    old_time = None
+    session_window_start = None
     logs = 0
 
-    with open(sys.argv[3],"w") as output_file:
+    with open(sys.argv[3], "w") as output_file:
         print("clean file")
 
-    with open(sys.argv[2],"r") as inactivity_time:
-        inactivity_time = int(inactivity_time.read())
+    with open(sys.argv[2], "r") as inactivity_time_file:
+        inactivity_time = int(inactivity_time_file.read())
+        inactivity_time_file.close()
 
-    #iterating through the input file.
-    with open(sys.argv[1],"r") as input_file:
+    # iterating through the input file.
+    with open(sys.argv[1], "r") as input_file:
         reader = csv.DictReader(input_file, delimiter=",")
         header = reader.fieldnames
         for i, line in enumerate(reader):
             current_time = convertTime(line['date'], line['time'])
-            if old_time == None:
-                old_time = current_time
-            #check to see if any files expired.
-            old_time = checkInactiveHeap(time_heaps, old_time, user_info,inactivity_time,current_time)
+            if session_window_start is None:
+                session_window_start = current_time
+            # check to see if any files expired.
+            session_window_start = advanceSessionWindow(
+                time_heaps, session_window_start, user_info, inactivity_time, current_time)
             ip = line['ip']
             if ip not in user_info:
-                user_data = UserData(current_time,  0, ip)
+                user_data = UserData(current_time, 0, ip)
                 user_info[ip] = user_data
             else:
                 user_data = user_info[ip]
             user_data.requests += 1
             user_data.end_time = current_time
-            #min heap to make sure files are outputted in start time order.
-            details = UserTime(line['ip'], user_data.requests,user_info[ip].logs,current_time)
+            # min heap to make sure files are outputted in start time order.
+            details = UserTime(
+                line['ip'],
+                user_info[ip].logs)
             if current_time not in time_heaps:
-                time_heaps[current_time] = []
-            heapq.heappush(time_heaps[current_time],details)
-            user_times.put(details)
-    checkNaiveInactive(user_times,inactivity_time, current_time,True)
-
-
-            
-
-            
-
+                time_heaps[current_time] = ([], set())
+            if ip not in time_heaps[current_time][1]:
+                heapq.heappush(time_heaps[current_time][0], details)
+                time_heaps[current_time][1].add(ip)
+    checkNaiveInactive(user_info, inactivity_time, current_time, True)
+    input_file.close()
